2beca5942af4b92c3b4bed4e967e971e3002d78b
Ariel Cooper <featherboa50@yahoo.com>
2022-11-01 12:04:53 -0700

    api key added
    pulls api key from install page and can call in app

--- Demo-Apps/serverless/server/server.js
+++ Demo-Apps/serverless/server/server.js
@@ -1,7 +1,16 @@
-function getCCList(ticket){
-	
+function getCC(args){
+	var request = [{
+		method: 'GET',
+		//url: `${args.domain}/api/v2/tickets/${args.ticket}`.
+		//apiKey: args.apiKey
+	}];
 }
 
+/*function putCC(){
+	
+}*/
+
+//if positive we want to pull cc from returned ticket, else push cc to returned ticket
 function getTicketfromDesc(desc) {
 	var r = /\d+/;
 	var ticket;
@@ -10,17 +19,16 @@ function getTicketfromDesc(desc) {
 	if (desc.toLowerCase().includes('merged from')){
 		ticket = parseInt(desc.match(r));
 		console.log("Child ticket:" + ticket);
-		
 	//child do i need this? or willit make a loop?
 	}else if(desc.toLowerCase().includes('merged into')){
-		ticket = parseInt(desc.match(r));
+		ticket = -(parseInt(desc.match(r)));
 		console.log("Parent ticket:" + ticket);
 	//Not a merge no further action needed
 	}else{
-		//ticket = 0;
+		ticket = 0;
 		console.log("Not merged");
 	}
-
+	return ticket;
 }
 
 exports = {
@@ -41,10 +49,21 @@ exports = {
 		}
     },
 	 onConversationCreateCallback: function (args) {
-		//console.log("Convo");
-		var description = args.data.conversation.body_text;
-		getTicketfromDesc(description);
+		const data = args.data,
+		key = args.iparams.apiKey
+		description = args.data.conversation.body_text;
+		var tick = getTicketfromDesc(description);
+		//console.log(iparams.apiKey);
 		
+		if(tick > 0){ //pull cc from tick
+			var cclist = getCC({
+				ticket: tick,
+				domain: args.domain,
+				apiKey: key
+			});
+		}else if(tick < 0){ //pull cc from arg
+			var cclist = args.data.cc_emails;
+		}
 		
 		//curl -v -u CnvwmbRJJe7zUowpWh:X -H "Content-Type: application/json" -X GET 'https://resideworldwide.freshservice.com/api/v2/tickets/11625'
 		
